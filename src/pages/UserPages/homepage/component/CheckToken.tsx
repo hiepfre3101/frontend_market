c68a3c83ff836aa856d43c8da8da72feb0d2368c
Linh-203 <hlinh2003.5.9@gmail.com>
2023-09-17 17:08:51 +0700

    update logout

--- src/pages/UserPages/homepage/component/CheckToken.tsx
+++ src/pages/UserPages/homepage/component/CheckToken.tsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom';
 import { Popover } from 'antd';
 import { CiUser } from 'react-icons/ci';
-import { useSelector } from 'react-redux';
-import { IAuth } from '../../../../slices/authSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { IAuth, deleteTokenAndUser } from '../../../../slices/authSlice';
+import { useClearTokenMutation } from '../../../../services/auth.service';
 
 const CheckToken = () => {
    const auth = useSelector((state: { userReducer: IAuth }) => state.userReducer);
+   const [clearToken] = useClearTokenMutation();
+   const dispatch = useDispatch();
+   const onHandleLogout = () => {
+      dispatch(deleteTokenAndUser());
+      clearToken();
+   };
    return (
       <div>
          {!auth?.accessToken ? (
@@ -33,7 +40,7 @@ const CheckToken = () => {
                      <>
                         <Link to={'/signup'}>Tài Khoản</Link>
                         <br />
-                        <Link to={'/login'}>Đăng xuất</Link>
+                        <button onClick={() => onHandleLogout()}>Đăng xuất</button>
                      </>
                   }
                   trigger='click'
